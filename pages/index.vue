6d292dad34015278ae10ec7d89b9944f8c065e7a
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-10-30 23:04:17 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,16 +13,25 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import axios from 'axios';
 
+const route = useRoute();
+const paramDate = route.query.date || '';
 const tweet = ref(null);
 
 onMounted(async () => {
   const response = await axios.get('/data.json');
   const data = await response.data.tweets;
-  const today = new Date();
+
+  const month = parseInt(paramDate.substring(0, 2), 10) - 1;
+  const day = parseInt(paramDate.substring(2, 4), 10);
+  let target = new Date(new Date().getFullYear(), month, day);
+  if (target.getMonth() !== month || target.getDate() !== day) {
+    target = new Date();
+  }
   const tweets = data
-    .filter(tweet => tweet.tweet.created_at.startsWith(`${today.getMonth() + 1}/${today.getDate()}`))
+    .filter(tweet => tweet.tweet.created_at.startsWith(`${(target.getMonth() + 1).toString().padStart(2, '0')}/${target.getDate().toString().padStart(2, '0')}`))
     .filter(tweet => !tweet.tweet.full_text.includes("@"));
   tweet.value = tweets[Math.floor(Math.random() * tweets.length)].tweet;
 });
