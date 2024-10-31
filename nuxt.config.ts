83c7a200050e927859e43d5f78bb8cb3788f2237
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-10-31 16:31:18 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,10 +11,9 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'format-detection', content: 'telephone=no' },
         { property: 'twitter:card', content: "summary" },
+        { property: 'og:title', content: 'my-tweet-history' },
         { property: 'og:description', content: '黒歴史bot' },
-        { property: 'og:image', content: './favicon.ico' },
-        { property: 'og:url', content: 'https://kuskyst.github.io/my-tweet-history/' },
-        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: './favicon.ico' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }
